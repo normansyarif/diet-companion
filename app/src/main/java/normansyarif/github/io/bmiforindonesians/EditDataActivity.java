a725bab3b85a4035b6087d8243c4b9f454bd84be
Norman Syarif <normansyarif@icloud.com>
2020-01-01 10:24:48 +0700

    Add password to edit data

--- app/src/main/java/normansyarif/github/io/bmiforindonesians/EditDataActivity.java
+++ app/src/main/java/normansyarif/github/io/bmiforindonesians/EditDataActivity.java
@@ -8,11 +8,12 @@ import android.os.Bundle;
 import android.preference.PreferenceManager;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.Toast;
 
 public class EditDataActivity extends AppCompatActivity {
 
     SharedPreferences pref;
-    EditText inputDates, inputData;
+    EditText inputDates, inputData, inputPassword;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,6 +23,7 @@ public class EditDataActivity extends AppCompatActivity {
         pref = PreferenceManager.getDefaultSharedPreferences(getApplicationContext());
         inputDates = findViewById(R.id.dates);
         inputData = findViewById(R.id.data);
+        inputPassword = findViewById(R.id.password);
 
         parseData();
     }
@@ -34,25 +36,29 @@ public class EditDataActivity extends AppCompatActivity {
     }
 
     public void onSaveClicked(View v) {
-        String iDates = inputDates.getText().toString();
-        String iData = inputData.getText().toString();
+        if(inputPassword.getText().toString().equals("Hmiku")) {
+            String iDates = inputDates.getText().toString();
+            String iData = inputData.getText().toString();
 
-        if(iDates.equals("")) {
-            iDates = "[]";
+            if(iDates.equals("")) {
+                iDates = "[]";
+            }
+
+            if(iData.equals("")) {
+                iData = "[]";
+            }
+
+            SharedPreferences.Editor editor = pref.edit();
+            editor.putString("dates", iDates);
+            editor.putString("data", iData);
+            editor.apply();
+
+            Intent intent = new Intent(EditDataActivity.this, ChartActivity.class);
+            startActivity(intent);
+            finish();
+        }else{
+            Toast.makeText(this, "Wrong password", Toast.LENGTH_SHORT).show();
         }
-
-        if(iData.equals("")) {
-            iData = "[]";
-        }
-
-        SharedPreferences.Editor editor = pref.edit();
-        editor.putString("dates", iDates);
-        editor.putString("data", iData);
-        editor.apply();
-
-        Intent intent = new Intent(EditDataActivity.this, ChartActivity.class);
-        startActivity(intent);
-        finish();
     }
 
     public void onClearClicked(View v) {
